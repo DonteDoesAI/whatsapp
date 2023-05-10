9bc1c9f7ea7ea1912b6ce69be0441ca39de57f65
jvbaker95 <jvbaker95@hotmail.com>
2023-05-10 18:09:04 -0400

    Added Chromium Downloader

---
+++ ChromiumDownloader.ps1
@@ -0,0 +1,41 @@
+# Get Chrome Version
+$ErrorActionPreference = "Stop"
+try {
+    $Chrome_Application = (
+        Get-Item `
+            (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)'
+    ).VersionInfo
+} catch {
+    Write-Error -Message "Can't find Chrome! Did you install it?"
+}
+
+$Chrome_Driver_Download_Page = Invoke-WebRequest -Uri "https://chromedriver.chromium.org/downloads"
+
+# Check what Chrome Drivers are available, using a discrete class.
+$hrefs = $Chrome_Driver_Download_Page.ParsedHtml.Links `
+    | Where-Object {$_.className -like "XqQF9c"} `
+        | Where-Object {$_.ie8_href -like "https://chromedriver.storage.googleapis.com/index.html?path=*"} `
+            | Select-Object -Property @( # Alias the fieldnames, where E is the original field.
+                @{N="Version";E={[System.Version]($_.innerText.replace("ChromeDriver","").trim())}}, # Cast as a version object
+                @{N="Link";E={$_.ie8_href}}
+            ) -Unique
+
+$valid_drivers = $hrefs `
+    | Where-Object {
+        $_.Version.Major ` # Select drivers where the major revision matches the version of Google Chrome Installed
+            -eq `
+        ([System.Version]$Chrome_Application.FileVersion).Major # Cast the Application as a System.Version Object
+    } `
+    | Sort-Object -Property Version -Descending # The most recent version is placed at the top of the list
+
+$selected_driver = $valid_drivers[0]
+
+$driver_uri = "https://chromedriver.storage.googleapis.com/$($selected_driver.Version)/chromedriver_win32.zip"
+$zip_path = $([System.IO.Path]::combine("utils","chromedriver_win32.zip"))
+# Download the correct chrome driver.
+
+$driver_page = Invoke-WebRequest `
+    -Uri $driver_uri `
+    -OutFile $zip_path
+
+Expand-Archive $zip_path -DestinationPath "utils"
